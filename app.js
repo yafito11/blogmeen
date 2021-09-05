13977d80e00a88060d477c6b2ef1b54972ea7907
yafito11 <yafiemember15@gmail.com>
2021-09-05 12:40:23 +0700

    templating dengan ejs view

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express()
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
 
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
 require('./src/routes/home.routes')(app)
 
 const PORT = 5000
