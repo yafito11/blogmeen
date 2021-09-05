0d3f73b4fe239afa621a6957463e8cd6b801c978
yafito11 <yafiemember15@gmail.com>
2021-09-05 14:48:55 +0700

    mendapatkan data dari form dengan form-urlencoded

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express()
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
 
+app.use(express.urlencoded({
+    extended: false
+}))
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
