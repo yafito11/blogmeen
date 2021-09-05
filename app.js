f513153da0d6bdb5b0d367e0370b5ab8661884d6
yafito11 <yafiemember15@gmail.com>
2021-09-05 08:20:23 +0700

    membuat file index

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
+require('./src/routes/home.routes')(app)
 
 const PORT = 5000
 app.listen(PORT, () => {
